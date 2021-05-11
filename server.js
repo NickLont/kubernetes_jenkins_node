d007d75e2d4e8ff9ddcfccc0effb1309cdbef86e
Nick Lontorfos <nlontorfos@hotmail.com>
2021-05-11 15:26:47 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const PORT = 8000;
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Guys!!');
+});
+app.listen(PORT);
+console.log(`Running on ${PORT}`);
